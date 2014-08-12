697e93aea9d104a2e70dfd8d8c5698b19006df10
Abhay Rana <capt.n3m0@gmail.com>
2014-08-12 17:02:45 +0530

    no need to explode string anymore

--- app/views.php
+++ app/views.php
@@ -3,8 +3,6 @@ global $twig;
 $loader = new Twig_Loader_Filesystem('app/views');
 $twig = new Twig_Environment($loader);
 $filter = new Twig_SimpleFilter('remove_first_word', function ($string) {
-    $arr = explode(' ',$string);
-    array_splice($arr, 0, 1);
-    return join(' ', $arr);
+    return substr(strstr($string," "), 1);
 });
 $twig->addFilter($filter);
