a4f5fd4d471c9829df4f7e672592080afc068e7e
Abhay Rana <capt.n3m0@gmail.com>
2014-09-10 00:28:54 +0530

    Updates item model to support item creation

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,7 +1,13 @@
 class ItemsController < ActionController::Base
   # This is creation of an item
   # from the slack webhook
-  def post
-    
+  def create
+    Item.create_from_webhook params
+    render json: {text: "Saved"}
+  end
+
+  def index
+    items = Item.all
+    render json: items
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,30 @@
 class Item < ActiveRecord::Base
+  belongs_to :team
+  belongs_to :channel
+  belongs_to :user
+  def self.create_from_webhook(params)
+    
+    # Get the corresponding team
+    team = Team.find_or_create_by(identifier: params[:team_id]) do |t|
+      t.name = params[:team_domain]
+    end
+
+    channel = Channel.find_or_create_by(identifier: params[:channel_id]) do |c|
+      c.name = params[:channel_name]
+    end
+
+    user = User.find_or_create_by(identifier: params[:user_id]) do |u|
+      u.name = params[:user_name]
+    end
+
+    text = params[:text].slice(params[:trigger_word].length..-1).lstrip
+
+    item = Item.create({
+      :team => team,
+      :channel => channel,
+      :user => user,
+      :timestamp => params[:timestamp].to_i,
+      :text => text
+    })
+  end
 end
