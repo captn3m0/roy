fcdfbf9e5b743a6219dda93f620490819c03584d
Abhay Rana <capt.n3m0@gmail.com>
2014-09-10 10:52:50 +0530

    Proper handling of slack response via a method in ApplicationController
    - Shifts to high_voltage for static pages
    - Uses I18n for slack success response messages.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
-  def index
-    render plain: "Roy welcomes you"
+  def render_slack(text)
+    render json: {text: text}
   end
 end
--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,14 @@
-class ItemsController < ActionController::Base
+class ItemsController < ApplicationController
+
+  # We'll instead use the auth token that slack provides
+  skip_before_filter :verify_authenticity_token, :only => [:create]
+
   # This is creation of an item
   # from the slack webhook
   def create
-    Item.create_from_webhook params
-    render json: {text: "Saved"}
+    item = Item.create_from_webhook params
+    message = I18n.t('item_create_response').sample
+    render_slack message
   end
 
   def index
