49259aba7964e7aae91b651ae3309181cc53a281
Abhay Rana <capt.n3m0@gmail.com>
2014-09-27 14:54:06 +0530

    Fixes set storage in session.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,9 +1,10 @@
 class SessionsController < ApplicationController
   def create
     user = User.create_from_oauth(auth_hash)
-    session[:team] ||= Set.new
+    session[:team] = session[:team].to_set || Set.new
     session[:team].add user.team
     session[:user] = user
+    redirect_to "/#{user.team.name}"
   end
 
   protected
